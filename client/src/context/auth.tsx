d57025a205a3d4e98e7ce57560fd37c3e1a69a7a
uxy0301@gmail.com <umeharakouhei@umeharakouheinoMac-mini.local>
2023-06-18 17:18:00 +0900

    [add]logout

--- client/src/context/auth.tsx
+++ client/src/context/auth.tsx
@@ -1,7 +1,8 @@
 import apiClient from "@/lib/apiClient";
-import React, { ReactNode, useContext,useEffect } from "react";
+import React, { ReactNode, useContext, useEffect, useState } from "react";
 
 interface AuthContextType {
+  user: null | { id: number; email: string; username: string };
   login: (token: string) => void;
   logout: () => void;
 }
@@ -11,38 +12,57 @@ interface AuthProviderProps {
 }
 
 const AuthContext = React.createContext<AuthContextType>({
+  user: null,
   login: () => {},
   logout: () => {},
 });
 
 export const useAuth = () => {
   return useContext(AuthContext);
-}
+};
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  
+  const [user, setUser] = useState<null | {
+    id: number;
+    email: string;
+    username: string;
+  }>(null);
   useEffect(() => {
     const token = localStorage.getItem("auth_token");
-    
-    apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
-
-
-
+    if (token) {
+      apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+      apiClient
+        .get("/users/find")
+        .then((res) => {
+          setUser(res.data.user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }, []);
+
   const login = async (token: string) => {
     localStorage.setItem("auth_token", token);
+    apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+    try {
+      apiClient.get("/users/find").then((res) => {
+        setUser(res.data.user);
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const logout = () => {
     localStorage.removeItem("auth_token");
+    delete apiClient.defaults.headers["Authorization"];
+    setUser(null);
   };
   const value = {
+    user,
     login,
     logout,
   };
-  return (
-    <AuthContext.Provider value={value}>
-      {children}
-    </AuthContext.Provider>
-  )
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
